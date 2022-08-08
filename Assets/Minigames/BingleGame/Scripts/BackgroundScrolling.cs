00715b14b77a04fcfd3012d243d211c3cfbacd7d
David-Donghyun-Kim <67942037+David-Donghyun-Kim@users.noreply.github.com>
2022-08-08 03:24:23 -0700

    돌아돌아 최초셋업

---
+++ Assets/Minigames/BingleGame/Scripts/BackgroundScrolling.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameHeaven.BingleGame
+{
+    public class BackgroundScrolling : MonoBehaviour
+    {
+        public float speed;
+        int startIndex = 0;
+        int endIndex = 2;
+        public Transform[] sprites;
+
+        float viewHeight;
+
+        private void Awake()
+        {
+            viewHeight = Camera.main.orthographicSize * 2;
+        }
+        void Update()
+        {
+            Vector3 curPos = transform.position;
+            Vector3 nextPos = Vector3.up * speed * Time.deltaTime;
+            transform.position = curPos + nextPos;
+
+            if (sprites[endIndex].position.y > viewHeight)
+            {
+                Vector3 backSpritePos = sprites[startIndex].localPosition;
+
+                sprites[endIndex].transform.localPosition = backSpritePos + Vector3.down * viewHeight;
+
+                int startIndexSave = startIndex;
+                startIndex = endIndex;
+                endIndex = (startIndexSave + 1 == sprites.Length) ? 0 : startIndexSave + 1;
+            }
+        }
+    }
+}
