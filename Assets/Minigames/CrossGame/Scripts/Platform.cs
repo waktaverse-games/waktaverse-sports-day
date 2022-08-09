f8ca21ff8a1387bf4ba69367808c217c18dea215
dmsrkfcl15 <dmsrkfcl15@naver.com>
2022-08-09 19:41:55 +0900

    기본 움직임 완성

--- Assets/Minigames/CrossGame/Scripts/Platform.cs
+++ Assets/Minigames/CrossGame/Scripts/Platform.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
-public class Platform : MonoBehaviour
+namespace GameHeaven.CrossGame
 {
-    // Start is called before the first frame update
-    void Start()
+    public class Platform : MonoBehaviour
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            transform.DOScale(new Vector3(1.2f, 0.8f), 0.5f).SetEase(Ease.OutBounce);
+        }
+
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+            Sequence sequence = DOTween.Sequence();
+            sequence.Append(transform.DOScale(new Vector3(0.8f, 1.5f), 0.2f).SetEase(Ease.OutBounce)).Append(transform.DOScale(new Vector3(1f, 1f), 0.5f).SetEase(Ease.OutBounce));
+        }
     }
 }
+
