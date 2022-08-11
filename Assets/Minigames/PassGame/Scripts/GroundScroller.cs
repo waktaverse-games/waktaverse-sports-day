efc30c3359e6e67645c9d35d72730ae47a93a11f
richard0326 <richard0326@naver.com>
2022-08-11 21:55:58 +0900

    1차 데모 완성

--- Assets/Minigames/PassGame/Scripts/GroundScroller.cs
+++ Assets/Minigames/PassGame/Scripts/GroundScroller.cs
@@ -7,60 +7,51 @@ namespace GameHeaven.PassGame
 { 
     public class GroundScroller : MonoBehaviour
     {
-        public GameObject TilePrefab = null;
         public float GroundSpeed = -1.0f;
 
-        private List<SpriteRenderer> _tiles = new List<SpriteRenderer>();
+        public List<SpriteRenderer> Tiles = new List<SpriteRenderer>();
+        
         private SpriteRenderer _lastTile;
         private const float DeadLine = -11.5f;
-        private const float TileSize = 1.0f;
+        private const float TileSize = 3.0f;
 
         void Start()
         {
-            if (TilePrefab)
-            {
-                InitTiles();
-            }
+            FindLastTile();
         }
 
+        void FindLastTile()
+        {
+            float x = Tiles[0].transform.position.x;
+            for (int i = 1; i < Tiles.Count; i++)
+            {
+                if (Tiles[i].transform.position.x > x)
+                {
+                    _lastTile = Tiles[i];
+                }
+            }
+        }
         void Update()
         {
-            if (TilePrefab)
-            {
-                UpdateTiles();
-            }
-        }
-
-        void InitTiles()
-        {
-            for (int i = 0; i < 23; i++)
-            {
-                SpriteRenderer addTile = Instantiate(TilePrefab.GetComponent<SpriteRenderer>(),
-                    new Vector3(-11f + i * DeadLine, -0.5f, 1f), Quaternion.identity);
-                    
-                addTile.transform.SetParent(this.transform);
-                _tiles.Add(addTile);
-            }
-
-            _lastTile = _tiles[_tiles.Count - 1];
+            UpdateTiles();
         }
         
         // Ÿ�� �̵�
         void UpdateTiles()
         {
-            for (int i = 0; i < _tiles.Count; i++)
+            for (int i = 0; i < Tiles.Count; i++)
             {
                 // �� ������ ���� �ڷ� �̵��ҰԿ�
-                if (DeadLine >= _tiles[i].transform.position.x)
+                if (DeadLine >= Tiles[i].transform.position.x)
                 {
-                    _tiles[i].transform.position = new Vector3(_lastTile.transform.position.x + TileSize, -0.5f, 1f);
-                    _lastTile = _tiles[i];
+                    Tiles[i].transform.position = new Vector3(_lastTile.transform.position.x + TileSize, -0.5f, 1f);
+                    _lastTile = Tiles[i];
                 }
             }
 
-            for (int i = 0; i < _tiles.Count; i++)
+            for (int i = 0; i < Tiles.Count; i++)
             {
-                _tiles[i].transform.Translate(new Vector2(GroundSpeed, 0) * Time.deltaTime);
+                Tiles[i].transform.Translate(new Vector2(GroundSpeed, 0) * Time.deltaTime);
             }
         }
     }
--- Assets/Minigames/PassGame/Scripts/PlayerController.cs
+++ Assets/Minigames/PassGame/Scripts/PlayerController.cs
@@ -1,18 +1,21 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 namespace GameHeaven.PassGame
 {
     public class PlayerController : MonoBehaviour
     {
+        // reference : https://www.youtube.com/watch?v=qWWYLdnoZkI&list=PLbkWuDuFAzKVjC0FxUHAJKn5Vxc3GrymV&index=2
         // reference : https://www.youtube.com/watch?v=RmqWuoFHD5g
     
         public float JumpForce = 6.0f;
 
         private LayerMask _groundLayer;
-        private LayerMask _MonsterLayer;
+        private LayerMask _MonsterHeadLayer;
+        private LayerMask _MonsterBodyLayer;
         private Rigidbody2D _rb2d;
         private BoxCollider2D _boxCollider2D;
         private int _jumpCount = 0;
@@ -25,7 +28,8 @@ namespace GameHeaven.PassGame
             _rb2d = GetComponent<Rigidbody2D>();
             _boxCollider2D = GetComponent<BoxCollider2D>();
             _groundLayer = LayerMask.GetMask("Ground");
-            _MonsterLayer = LayerMask.GetMask("Monster");
+            _MonsterHeadLayer = LayerMask.GetMask("MonsterHead");
+            _MonsterBodyLayer = LayerMask.GetMask("MonsterBody");
         }
 
         private void Update()
@@ -58,7 +62,7 @@ namespace GameHeaven.PassGame
             {
                 _jumpCount = 0;
             }
-            else if (_isSpaceUp && Physics2D.OverlapCircle(_footPosistion, 0.01f, _MonsterLayer))
+            else if (_isSpaceUp && Physics2D.OverlapCircle(_footPosistion, 0.01f, _MonsterHeadLayer))
             {
                 SetJump();
             }
@@ -75,6 +79,16 @@ namespace GameHeaven.PassGame
         void SetJump()
         {
             _rb2d.velocity = Vector2.up * JumpForce;
+        }
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            if ((1 << collision.collider.gameObject.layer) == _MonsterBodyLayer.value)
+            {
+                Debug.Log("End");
+                // will be fixed
+                Application.Quit();
+            }
         }
     }
 }
