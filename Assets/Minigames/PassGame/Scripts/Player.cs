895b2ea0ceb7f1f8364d749b951800f856ffd9e0
streetmeow <joohee2008@cau.ac.kr>
2022-08-20 18:21:24 +0900

    플레이어 움직임 구현

---
+++ Assets/Minigames/PassGame/Scripts/Player.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D _rigid;
+    private bool _isGrounded = false;
+    private Animator _anim;
+    
+    public float jumpPower = 30.0f;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        _rigid = GetComponent<Rigidbody2D>();
+        _anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Jump();
+    }
+
+    void Jump()
+    {
+        if (Input.GetKey(KeyCode.Space) && _isGrounded)
+        {
+            _rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+            _anim.SetBool("isJump", true);
+            _isGrounded = false;
+        }
+    }
+    
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("Bottom"))
+        {
+            _isGrounded = true;
+            _anim.SetBool("isJump", false);
+        }
+    }
+}
