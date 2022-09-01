ea6d8cfca8b09c218f631ed6bef3843fd0aad7fc
zhdlxh48 <zhdlxh48@gmail.com>
2022-09-01 23:44:51 +0900

    Improve Block detection

--- Assets/Minigames/PunctureGame/Scripts/PlayerController.cs
+++ Assets/Minigames/PunctureGame/Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using GameHeaven.PunctureGame.Utils;
+using UnityEngine;
 
 namespace GameHeaven.PunctureGame
 {
@@ -6,42 +7,32 @@ namespace GameHeaven.PunctureGame
     {
         [SerializeField] private float moveSpeed = 1.0f;
         [SerializeField] private bool isFacingLeft = true;
+        [SerializeField] private float groundDetectDistance = 0.1f;
         [SerializeField] private float blockDetectDistance = 1.0f;
         [SerializeField] private float blockDetectGap = 0.1f;
 
         [SerializeField] private Rigidbody2D playerRigid;
-        [SerializeField] private Block detectedBlock = null;
         [SerializeField] private LayerMask brickLayerMask;
-        private Block DetectedBlock
-        {
-            get => detectedBlock;
-            set
-            {
-                if (detectedBlock)
-                {
-                    if (!detectedBlock.Equals(value))
-                    {
-                        detectedBlock = value;
-                    }
-                }
-                else
-                {
-                    detectedBlock = value;
-                }
-            }
-        }
 
-        // Update is called once per frame
+        private Vector3 PlayerPos => transform.position;
+        private Vector3 PlayerScale => transform.localScale;
+
+        private RaycastHit2D GroundHit =>
+            Physics2D.CircleCast(PlayerPos, groundDetectDistance, Vector2.zero, 0f, brickLayerMask);
+
         private void Update()
         {
-            DetectedBlock = DetectBlock();
-
             // transform.Translate(moveSpeed * (isFacingLeft ? -1 : 1) * Time.deltaTime, 0.0f, 0.0f);
             playerRigid.velocity = new Vector2(moveSpeed * (isFacingLeft ? -1 : 1), playerRigid.velocity.y);
 
             if (Input.GetKeyDown(KeyCode.C))
             {
-                DetectedBlock.Crash();
+                var hitInfo = BottomBlockHit();
+                if (hitInfo)
+                {
+                    var block = hitInfo.transform.GetComponent<Block>();
+                    block.Crash();
+                }
             }
 
             if (Input.GetKeyDown(KeyCode.Space))
@@ -49,40 +40,39 @@ namespace GameHeaven.PunctureGame
                 isFacingLeft = !isFacingLeft;
             }
         }
-
-        private Block DetectBlock()
-        {
-            var playerScale = transform.localScale;
-            var playerPos = transform.position;
-            
-            var fixVecWithGap = new Vector3(playerScale.x / 2.0f + blockDetectGap, 0.0f);
-            var pivot = playerPos - new Vector3(0.0f, playerScale.y / 2.0f) + (isFacingLeft ? -fixVecWithGap : fixVecWithGap);
-            
-            var hitInfo = Physics2D.Raycast(pivot, Vector3.down, blockDetectDistance, brickLayerMask);
-            return hitInfo ? hitInfo.transform.GetComponent<Block>() : null;
+        
+        /// <summary>
+        /// Raycast를 통해 아래의 블록의 hit 정보를 가져온다
+        /// </summary>
+        /// <returns>아래 블록의 hit 정보</returns>
+        private RaycastHit2D BottomBlockHit() {
+            var fixVecWithGap = new Vector3(PlayerScale.x / 2.0f + blockDetectGap, 0.0f);
+            var pivot = PlayerPos + (isFacingLeft ? -fixVecWithGap : fixVecWithGap);
+                
+            return Physics2D.Raycast(pivot, Vector3.down, blockDetectDistance, brickLayerMask);
         }
 
 #if UNITY_EDITOR
         private void OnDrawGizmos()
         {
-            var playerScale = transform.localScale;
-            var playerPos = transform.position;
-            
             // Player Scale
-            Gizmos.color = new Color(0.7f, 0.1f, 0.1f, 0.4f);
-            Gizmos.DrawCube(playerPos, playerScale);
+            GizmosUtil.DrawCube(PlayerPos + new Vector3(0.0f, PlayerScale.y / 2.0f), PlayerScale, new Color(0.7f, 0.1f, 0.1f, 0.4f));
+
+            // Jump Detect Area
+            GizmosUtil.DrawWireSphere(PlayerPos, groundDetectDistance, GroundHit ? Color.cyan : Color.blue);
             
-            var fixVecWithGap = new Vector3(playerScale.x / 2.0f + blockDetectGap, 0.0f);
-            var pivot = playerPos - new Vector3(0.0f, playerScale.y / 2.0f) + (isFacingLeft ? -fixVecWithGap : fixVecWithGap);
+            var fixVecWithGap = new Vector3(PlayerScale.x / 2.0f + blockDetectGap, 0.0f);
+            var pivot = PlayerPos + (isFacingLeft ? -fixVecWithGap : fixVecWithGap);
             
-            Gizmos.color = Color.green;
-            Gizmos.DrawLine(pivot, pivot + Vector3.down * blockDetectDistance);
-            var hitInfo = Physics2D.Raycast(pivot, Vector3.down, blockDetectDistance, brickLayerMask);
+            // Block Detect Ray
+            GizmosUtil.DrawString("detect ray", pivot, Color.white, Vector2.up);
+            GizmosUtil.DrawLine(pivot, pivot + Vector3.down * blockDetectDistance, Color.green);
+            
+            // Block Detected Sphere
+            var hitInfo = BottomBlockHit();
             if (hitInfo)
             {
-                Gizmos.color = Color.yellow;
-                Gizmos.DrawWireSphere(hitInfo.point, 0.2f);
-                Debug.Log($"{hitInfo.collider.name}: {hitInfo.point}");
+                GizmosUtil.DrawWireSphere(hitInfo.point, 0.2f, Color.yellow);
             }
         }
 #endif
