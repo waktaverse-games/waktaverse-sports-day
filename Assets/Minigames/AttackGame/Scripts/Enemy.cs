accbb588267a8491847a4a2d2cfe45a24468abbd
streetmeow <joohee2008@cau.ac.kr>
2022-09-12 02:59:58 +0900

    주스, 라니, 팬지

--- Assets/Minigames/AttackGame/Scripts/Enemy.cs
+++ Assets/Minigames/AttackGame/Scripts/Enemy.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 namespace GameHeaven.AttackGame
 {
@@ -15,10 +16,14 @@ namespace GameHeaven.AttackGame
 
         private int currentHp;
         private string _name;
+        private Animator _animator;
+        private SpriteRenderer _spriteRenderer;
 
         private void Start()
         {
             _name = gameObject.name;
+            _animator = GetComponent<Animator>();
+            _spriteRenderer = GetComponent<SpriteRenderer>();
         }
 
         void OnEnable()
@@ -40,8 +45,51 @@ namespace GameHeaven.AttackGame
 
         public void ActivateMovement()
         {
-            
+            switch (_name)
+            {
+                case "monkey":
+                    return;
+                case "fox":
+                    StartCoroutine(FoxMove());
+                    break;
+                case "gorani":
+                    StartCoroutine(GoraniToLeft());
+                    break;
+            }
         }
+
+        IEnumerator FoxMove()
+        {
+            yield return new WaitForSeconds(0.9f);
+            _animator.SetBool("isMove", true);
+            StartCoroutine(FoxStop());
+            transform.DOLocalMoveX(transform.position.x - 1.5f, 1.2f);
+        }
+
+        IEnumerator FoxStop()
+        {
+            yield return new WaitForSeconds(1f);
+            
+            _animator.SetBool("isMove", false);
+            StartCoroutine(FoxMove());
+        }
+
+        IEnumerator GoraniToLeft()
+        {
+            yield return new WaitForSeconds(1.1f);
+            _spriteRenderer.flipX = false;
+            transform.DOLocalMoveX(transform.position.x - 1.5f, 1f);
+            StartCoroutine(GoraniToRight());
+        }
+
+        IEnumerator GoraniToRight()
+        {
+            yield return new WaitForSeconds(1.1f);
+            _spriteRenderer.flipX = true;
+            transform.DOLocalMoveX(transform.position.x + 1.5f, 1f);
+            StartCoroutine(GoraniToLeft());
+        }
+        
     }
 }
 
