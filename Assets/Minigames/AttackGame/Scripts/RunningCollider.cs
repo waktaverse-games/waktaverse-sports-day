827be87db466e0f28c27d31a4d7195b6c3e0abcb
streetmeow <joohee2008@cau.ac.kr>
2022-09-11 17:11:13 +0900

    몬스터 헬스바

--- Assets/Minigames/AttackGame/Scripts/RunningCollider.cs
+++ Assets/Minigames/AttackGame/Scripts/RunningCollider.cs
@@ -15,7 +15,12 @@ namespace GameHeaven.AttackGame
                 Physics2D.IgnoreCollision(obj.GetComponent<BoxCollider2D>(), GetComponent<BoxCollider2D>());
             }
         }
-        
+
+        private void Start()
+        {
+            if (GetComponent<Renderer>().isVisible) Debug.Log("true");
+            else Debug.Log("false");
+        }
     }
 }
 
