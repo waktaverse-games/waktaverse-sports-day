19d9589b50aaaaf9a0cfc3246e7edc8475ddbe4a
streetmeow <joohee2008@cau.ac.kr>
2022-09-16 02:24:40 +0900

    namespace

--- Assets/Minigames/PassGame/Scripts/EnemyPattern.cs
+++ Assets/Minigames/PassGame/Scripts/EnemyPattern.cs
@@ -4,115 +4,119 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 
-public class EnemyPattern : MonoBehaviour
+namespace GameHeaven.PassGame
 {
-    private SpriteRenderer _spriteRenderer;
-    private Animator _animator;
-    private float _count = 1;
-    private void Awake()
+    public class EnemyPattern : MonoBehaviour
     {
-        _spriteRenderer = GetComponent<SpriteRenderer>();
-        _animator = GetComponent<Animator>();
-    }
+        private SpriteRenderer _spriteRenderer;
+        private Animator _animator;
+        private float _count = 1;
 
-    // Start is called before the first frame update
-    void OnEnable()
-    {
-        _count = 1;
-        switch (gameObject.name)
+        private void Awake()
         {
-            case "segyun":
-                StartCoroutine(Segyun(1f));
-                break;
-            case "gorani":
-                StartCoroutine(Gorani(1.5f));
-                break;
-            case "fox":
-                _animator.SetBool("isPause", false);
-                StartCoroutine(Fox(1.5f));
-                break;
-            case "ddulgi":
-                _animator.SetBool("isFly", false);
-                StartCoroutine(Ddulgi());
-                break;
-            case "dog":
-                StartCoroutine(Dog());
-                break;
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+            _animator = GetComponent<Animator>();
         }
-    }
 
-    private void OnDisable()
-    {
-        StopAllCoroutines();
-        transform.position = transform.parent.position;
-        _spriteRenderer.flipX = false;
-    }
-
-    IEnumerator Panchi()
-    {
-        yield break;
-    }
-
-    IEnumerator Segyun(float time)
-    {
-        yield return new WaitForSeconds(time);
-        transform.DOJump(transform.parent.position + new Vector3(-3, 0, 0),1, 1, 1);
-        StartCoroutine(Segyun(1f));
-    }
-
-    IEnumerator Gorani(float time)
-    {
-        yield return new WaitForSeconds(time);
-        if (transform.parent.position.x > -8f)
+        // Start is called before the first frame update
+        void OnEnable()
         {
-            _spriteRenderer.flipX = true;
-            transform.DOLocalMoveX(3f, 0.5f);
-            StartCoroutine(GoraniLeft(0.8f));
+            _count = 1;
+            switch (gameObject.name)
+            {
+                case "segyun":
+                    StartCoroutine(Segyun(1f));
+                    break;
+                case "gorani":
+                    StartCoroutine(Gorani(1.5f));
+                    break;
+                case "fox":
+                    _animator.SetBool("isPause", false);
+                    StartCoroutine(Fox(1.5f));
+                    break;
+                case "ddulgi":
+                    _animator.SetBool("isFly", false);
+                    StartCoroutine(Ddulgi());
+                    break;
+                case "dog":
+                    StartCoroutine(Dog());
+                    break;
+            }
         }
-    }
 
-    IEnumerator GoraniLeft(float time)
-    {
-        yield return new WaitForSeconds(time);
-        _spriteRenderer.flipX = false;
-        transform.DOLocalMoveX(-1.5f, 1);
-        StartCoroutine(Gorani(1.3f));
-    }
+        private void OnDisable()
+        {
+            StopAllCoroutines();
+            transform.position = transform.parent.position;
+            _spriteRenderer.flipX = false;
+        }
 
-    IEnumerator Fox(float time)
-    {
-        yield return new WaitForSeconds(time);
-        Invoke("FoxGo", 2.2f);
-        Invoke("FoxStop", 0.7f);
-        transform.DOLocalMoveX(-3.7f * _count, 1);
-        _count++;
-        StartCoroutine(Fox(2.5f));
-    }
+        IEnumerator Panchi()
+        {
+            yield break;
+        }
 
-    void FoxStop()
-    {
-        _animator.SetBool("isPause", true);
-    }
+        IEnumerator Segyun(float time)
+        {
+            yield return new WaitForSeconds(time);
+            transform.DOJump(transform.parent.position + new Vector3(-3, 0, 0), 1, 1, 1);
+            StartCoroutine(Segyun(1f));
+        }
 
-    void FoxGo()
-    {
-        _animator.SetBool("isPause", false);
-    }
+        IEnumerator Gorani(float time)
+        {
+            yield return new WaitForSeconds(time);
+            if (transform.parent.position.x > -8f)
+            {
+                _spriteRenderer.flipX = true;
+                transform.DOLocalMoveX(3f, 0.5f);
+                StartCoroutine(GoraniLeft(0.8f));
+            }
+        }
 
-    IEnumerator Ddulgi()
-    {
-        yield return new WaitForSeconds(3f);
-        _animator.SetBool("isFly", true);
-        Invoke("DdulgiMove", 0.3f);
-    }
+        IEnumerator GoraniLeft(float time)
+        {
+            yield return new WaitForSeconds(time);
+            _spriteRenderer.flipX = false;
+            transform.DOLocalMoveX(-1.5f, 1);
+            StartCoroutine(Gorani(1.3f));
+        }
 
-    void DdulgiMove()
-    {
-        transform.DOLocalMoveX(-10f, 4);
-    }
+        IEnumerator Fox(float time)
+        {
+            yield return new WaitForSeconds(time);
+            Invoke("FoxGo", 2.2f);
+            Invoke("FoxStop", 0.7f);
+            transform.DOLocalMoveX(-3.7f * _count, 1);
+            _count++;
+            StartCoroutine(Fox(2.5f));
+        }
 
-    IEnumerator Dog()
-    {
-        yield return new WaitForSeconds(1);
+        void FoxStop()
+        {
+            _animator.SetBool("isPause", true);
+        }
+
+        void FoxGo()
+        {
+            _animator.SetBool("isPause", false);
+        }
+
+        IEnumerator Ddulgi()
+        {
+            yield return new WaitForSeconds(3f);
+            _animator.SetBool("isFly", true);
+            Invoke("DdulgiMove", 0.3f);
+        }
+
+        void DdulgiMove()
+        {
+            transform.DOLocalMoveX(-10f, 4);
+        }
+
+        IEnumerator Dog()
+        {
+            yield return new WaitForSeconds(1);
+        }
     }
 }
--- Assets/Minigames/PassGame/Scripts/Player.cs
+++ Assets/Minigames/PassGame/Scripts/Player.cs
@@ -3,52 +3,57 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+namespace GameHeaven.PassGame
 {
-    private Rigidbody2D _rigid;
-    private bool _isGrounded = false;
-    private Animator _anim;
-    
-    public float jumpPower = 3.0f;
-    public GameManager gameManager;
-    // Start is called before the first frame update
-    void Awake()
+    public class Player : MonoBehaviour
     {
-        _rigid = GetComponent<Rigidbody2D>();
-        _anim = GetComponent<Animator>();
-    }
+        private Rigidbody2D _rigid;
+        private bool _isGrounded = false;
+        private Animator _anim;
 
-    // Update is called once per frame
-    void Update()
-    {
-        Jump();
-    }
+        public float jumpPower = 3.0f;
 
-    void Jump()
-    {
-        if (Input.GetKey(KeyCode.Space) && _isGrounded)
+        public GameManager gameManager;
+
+        // Start is called before the first frame update
+        void Awake()
         {
-            _rigid.velocity = Vector2.up * jumpPower;
-            _anim.SetBool("isJump", true);
-            _isGrounded = false;
+            _rigid = GetComponent<Rigidbody2D>();
+            _anim = GetComponent<Animator>();
         }
-    }
 
-    private void OnCollisionEnter2D(Collision2D col)
-    {
-        if (col.gameObject.CompareTag("Bottom"))
+        // Update is called once per frame
+        void Update()
         {
-            _isGrounded = true;
-            _anim.SetBool("isJump", false);
+            Jump();
         }
-    }
 
-    private void OnTriggerEnter2D(Collider2D col)
-    {
-        if (col.CompareTag("Enemy"))
+        void Jump()
         {
-            _rigid.velocity = Vector2.up * jumpPower;
-            gameManager.AddScore(15);
+            if (Input.GetKey(KeyCode.Space) && _isGrounded)
+            {
+                _rigid.velocity = Vector2.up * jumpPower;
+                _anim.SetBool("isJump", true);
+                _isGrounded = false;
+            }
+        }
+
+        private void OnCollisionEnter2D(Collision2D col)
+        {
+            if (col.gameObject.CompareTag("Bottom"))
+            {
+                _isGrounded = true;
+                _anim.SetBool("isJump", false);
+            }
+        }
+
+        private void OnTriggerEnter2D(Collider2D col)
+        {
+            if (col.CompareTag("Enemy"))
+            {
+                _rigid.velocity = Vector2.up * jumpPower;
+                gameManager.AddScore(15);
+            }
         }
     }
 }
