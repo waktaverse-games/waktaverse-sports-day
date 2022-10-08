5cd89f858092d70459747fdd3d59a58f23d19e29
David-Donghyun-Kim <67942037+David-Donghyun-Kim@users.noreply.github.com>
2022-10-07 22:37:52 -0700

    기초셋업

--- Assets/Minigames/JumpGame/Scripts/PlayerController.cs
+++ Assets/Minigames/JumpGame/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ namespace GameHeaven.JumpGame
     public class PlayerController : MonoBehaviour
     {
         public float maxSpeed;
+        public float jumpPower;
         Rigidbody2D rb;
         SpriteRenderer sprite;
         Animator animator;
@@ -24,20 +25,33 @@ namespace GameHeaven.JumpGame
             {
                 rb.velocity = new Vector2(rb.velocity.normalized.x * 0.5f, rb.velocity.y);
             }
-
+            if(Input.GetButtonDown("Jump") && !animator.GetBool("isJumping"))
+            {
+                rb.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+                animator.SetBool("isJumping", true);
+            }
+            if(rb.velocity.y == 0)
+            {
+                animator.SetBool("isJumping", false);
+            }
             UpdateFace();
         }
         private void FixedUpdate()
         {
             PlayerMovement();
+            PlayerLanding();
         }
 
         void UpdateFace()
         {
-            if (rb.velocity.x > 0)
-                sprite.flipX = true;
-            else
+            if(Input.GetKeyDown(KeyCode.A))
+            {
                 sprite.flipX = false;
+            }
+            else if(Input.GetKeyDown(KeyCode.D))
+            {
+                sprite.flipX = true; 
+            }
         }
 
         void PlayerMovement()
@@ -52,6 +66,22 @@ namespace GameHeaven.JumpGame
                 animator.SetBool("isRunning", true);
             else
                 animator.SetBool("isRunning", false);
+        }
+
+        void PlayerLanding()
+        {
+            if (rb.velocity.y < 0)
+            {
+                Debug.DrawRay(rb.position, Vector3.down, new Color(1, 0, 0));
+                RaycastHit2D rayHit = Physics2D.Raycast(rb.position, Vector3.down, 1, LayerMask.GetMask("Ground"));
+                if (rayHit.collider != null)
+                {
+                    if (rayHit.distance < 1f)
+                    {
+                        animator.SetBool("isJumping", false);
+                    }
+                }
+            }
         }
     }
 }
