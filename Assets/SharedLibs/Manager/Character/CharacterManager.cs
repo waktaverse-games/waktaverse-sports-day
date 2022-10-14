8ae8512189ad20b12f6b09e3308108e8b06eb3be
zhdlxh48 <zhdlxh48@gmail.com>
2022-10-14 23:28:01 +0900

    ScoreManager, CharacterManager 생성

---
+++ Assets/SharedLibs/Manager/Character/CharacterManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace SharedLibs.Character
+{
+    public class CharacterManager : MonoSingleton<CharacterManager>
+    {
+        [System.Serializable]
+        private class CharacterDictionary : UnitySerializedDictionary<CharacterType, CharacterData> {}
+        
+        [SerializeField] private CharacterDictionary characterDic;
+        
+        public CharacterType CurrentCharacter { get; set; }
+        public CharacterData CurrentCharacterData => characterDic[CurrentCharacter] == null ? null : characterDic[CurrentCharacter];
+
+        private void Awake()
+        {
+            characterDic = new CharacterDictionary();
+        }
+    }
+}
