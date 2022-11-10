f0ec17265d48e05a7a751482033aa325e7bdf056
streetmeow <joohee2008@cau.ac.kr>
2022-11-10 22:36:40 +0900

    attack animation

---
+++ Assets/Minigames/AttackGame/Scripts/ChangeCharacter.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SharedLibs;
+using UnityEditor.Animations;
+using UnityEngine.UI;
+
+namespace GameHeaven.AttackGame
+{
+    public class ChangeCharacter : MonoBehaviour
+    {
+        Animator anim;
+        [SerializeField]
+        AnimatorController[] controllers;
+        [SerializeField]
+        Sprite[] sprites;
+
+        [SerializeField] Image image;
+        [SerializeField] CharacterType currChar;
+        private void Awake()
+        {
+            anim = GetComponent<Animator>();
+
+        }
+        private void Start()
+        {
+            //var currChar = SharedLibs.Character.CharacterManager.Instance.CurrentCharacter;
+            ChooseCharacter(currChar);
+            image.sprite = sprites[(int)currChar];
+            var tempColor = image.color;
+            tempColor.a = 255f;
+            image.color = tempColor;
+        }
+
+        void ChooseCharacter(CharacterType type)
+        {
+            anim.runtimeAnimatorController = controllers[(int)type];
+        }
+    }
+}
