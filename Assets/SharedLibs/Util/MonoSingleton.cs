bf67170febbc6ad3631f18bb1ebb3eaf2b1c6ffd
zhdlxh48 <zhdlxh48@gmail.com>
2022-11-23 15:21:44 +0900

    CharacterManager, MonoSingleton Edit

--- Assets/SharedLibs/Util/MonoSingleton.cs
+++ Assets/SharedLibs/Util/MonoSingleton.cs
@@ -1,96 +1,102 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
+
+// https://gist.github.com/zhdlxh48/fcb4b8ff9d5bbbe755c4bba0e4f4af0d
 
 namespace SharedLibs
 {
     /// <summary>
-    /// Inherit from this base class to create a singleton.
-    /// e.g. public class MyClassName : Singleton<MyClassName> {}
+    ///     Mono singleton Class. Extend this class to make singleton component.
+    ///     Example:
+    ///     <code>
+    /// public class Foo : MonoSingleton<Foo>
+    /// </code>
+    ///     . To get the instance of Foo class, use <code>Foo.instance</code>
+    ///     Override <code>Init()</code> method instead of using <code>Awake()</code>
+    ///     from this class.
     /// </summary>
-    public abstract class MonoSingleton<T> : MonoBehaviour where T : MonoBehaviour
+    public abstract class MonoSingleton<T> : MonoBehaviour where T : MonoSingleton<T>
     {
-        // Check to see if we're about to be destroyed
-        // private static bool m_ShuttingDown = false;
-        // private static object m_Lock = new object();
-        private static T m_Instance;
+        // Query this to see if the singleton has been instantiated anywhere,
+        // since querying .Instance will create an instance automatically
+        // NOTE: Static field in generic type - the value will be different for each inheritance of MonoSingleton
+        public static bool Exists;
 
-        /// <summary>
-        /// Access singleton instance through this propriety.
-        /// </summary>
+        private static T instance;
+
         public static T Instance
         {
             get
             {
-                // Debug.Log(m_ShuttingDown);
-                // if (m_ShuttingDown)
-                // {
-                //     Debug.LogWarning("[Singleton] Instance '" + typeof(T) +
-                //                      "' already destroyed. Returning null.");
-                //     return null;
-                // }
-                //
-                // lock (m_Lock)
-                // {
-                    if (m_Instance == null)
-                    {
-                        m_Instance = GetInstance();
-                    }
- 
-                    return m_Instance;
-                // }
-            }
-        }
-
-        private void Awake() {
-            m_Instance = GetInstance();
-            if (m_Instance)
-            {
-                DontDestroyOnLoad(gameObject);
-            }
-            
-            Init();
-        }
-
-        private static T GetInstance()
-        {
-            // Search for existing instance.
-            var instances = FindObjectsOfType<T>();
- 
-            // Create new instance if one doesn't already exist.
-            if (instances.Length == 0)
-            {
-                // Need to create a new GameObject to attach the singleton to.
-                var singletonObject = new GameObject(typeof(T).Name);
-                m_Instance = singletonObject.AddComponent<T>();
-                singletonObject.name = typeof(T).ToString() + " (Singleton)";
- 
-                // Make instance persistent.
-                DontDestroyOnLoad(singletonObject);
-            }
-            else if (instances.Length > 1)
-            {
-                for (int i = 1; i < instances.Length; i++)
+                // Crazy null check to support calling this from outside the main thread
+                if (ReferenceEquals(instance, null))
                 {
-                    Destroy(instances[i].gameObject);
+                    instance = FindObjectOfType(typeof(T)) as T;
+
+                    // Object not found - create one
+                    if (instance == null)
+                    {
+#if UNITY_EDITOR
+                        //Debug.Log(typeof(T) + " instance was requested and not found. Creating one.");
+#endif
+                        CreateInstance();
+                    }
+
+                    if (instance == null) Debug.LogError("Could not get or create an instance of " + typeof(T));
+                }
+
+                return instance;
+            }
+        }
+
+        // #### UNITY INTERNAL METHODS ####
+
+        // Assign if this is the first instance, else just show a warning that we have more than one
+        protected void Awake()
+        {
+            if (instance == null)
+            {
+                instance = this as T;
+                if (instance != null)
+                {
+                    Exists = true;
+                    DontDestroyOnLoad(instance);
+                    instance.Init();
+                }
+                else
+                {
+                    Debug.LogError("Could not assign the instance of this class (" + typeof(T) + ")");
                 }
             }
-
-            return instances[0];
+            else
+            {
+                Debug.LogWarning("Multiple instances of the MonoSingleton class " + typeof(T) + " created.");
+            }
         }
 
-        /// <summary>
-        /// Use this method instead of "Awake", because of DontDestroyOnLoad
-        /// </summary>
+        // #### PROTECTED/PRIVATE METHODS ####
+
+        protected void OnDestroy()
+        {
+            if (instance != null) instance.SingletonDestroy();
+            instance = null;
+            Exists = false;
+        }
+
+
+        // #### PUBLIC METHODS ####
+
+        public static void CreateInstance()
+        {
+            if (instance != null) return;
+            instance = new GameObject("Instance of " + typeof(T), typeof(T)).GetComponent<T>();
+        }
+
+        // Substitute for Awake() in implementing classes
         public abstract void Init();
- 
-        // private void OnApplicationQuit()
-        // {
-        //     m_ShuttingDown = true;
-        // }
-        
-        // private void OnDestroy()
-        // {
-        //     m_ShuttingDown = true;
-        // }
+
+        // Called on OnDestroy() for implementing classes to perform any necessary cleanup
+        protected virtual void SingletonDestroy()
+        {
+        }
     }
 }
