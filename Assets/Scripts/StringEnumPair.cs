f3aae4ea22e2ab2705080731feef259fccf873ee
zhdlxh48 <zhdlxh48@gmail.com>
2022-11-25 06:33:45 +0900

    GameMode, Minigame select script added

---
+++ Assets/Scripts/StringEnumPair.cs
@@ -0,0 +1,18 @@
+﻿namespace DefaultNamespace
+{
+    [System.Serializable]
+    public class StringEnumPair<T> where T : System.Enum
+    {
+        private string typeString;
+        private T type;
+
+        public StringEnumPair(string typeString, T type)
+        {
+            this.typeString = typeString;
+            this.type = type;
+        }
+
+        public string TypeString => typeString;
+        public T Type => type;
+    }
+}
