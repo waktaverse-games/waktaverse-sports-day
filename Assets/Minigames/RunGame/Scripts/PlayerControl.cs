a612fcd0e133bbf835f4ed348b3824c21dfe4216
Ryu <ryush9706@gmail.com>
2022-12-02 20:02:00 +0900

    피드백작업2
    1. 풍신 아이템 이펙트 추가 및 애니메이션 수정
    
    2. 사운드 적용.

--- Assets/Minigames/RunGame/Scripts/PlayerControl.cs
+++ Assets/Minigames/RunGame/Scripts/PlayerControl.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using SharedLibs;
 
 namespace GameHaven.RunGame
 {
@@ -8,6 +9,7 @@ namespace GameHaven.RunGame
     {
 
         public GameObject Caracter;
+        public GameObject Effect;
         public GameObject Cam;
         public GameManager gameManager;
 
@@ -54,20 +56,17 @@ namespace GameHaven.RunGame
                     {
                         Dirleft = false; //왼쪽X
                         dir = Vector2.right; //오른쪽으로 이동
-                        if (Cam.GetComponent<Transform>().rotation.z <= -55 && Cam.GetComponent<Transform>().rotation.z > -140)
-                        {
-                            Caracter.GetComponent<SpriteRenderer>().flipX = false; //캐릭터 좌측
-                        }
-                        else
-                        {
-                            Caracter.GetComponent<SpriteRenderer>().flipX = true; // 캐릭터 우측
-                        }
+                        Caracter.GetComponent<SpriteRenderer>().flipX = true; //캐릭터 좌측
+                        Effect.GetComponent<SpriteRenderer>().flipX = false; //캐릭터 좌측
+                        
                     }
                     else
                     {
                         Dirleft = true;
                         dir = Vector2.left;
                         Caracter.GetComponent<SpriteRenderer>().flipX = false;
+                        Effect.GetComponent<SpriteRenderer>().flipX = true; //캐릭터 좌측
+
                     }
                 }
 
@@ -75,6 +74,7 @@ namespace GameHaven.RunGame
                 {
                     ItemTime += Time.deltaTime;
                     Caracter.GetComponent<CapsuleCollider2D>().enabled = false;
+                    Effect.SetActive(true);
 
                     if (ItemTime > 2 && ItemTime <= 2.7)
                     {
@@ -88,7 +88,8 @@ namespace GameHaven.RunGame
                         ItemTime = 0;
                         Caracter.GetComponent<CapsuleCollider2D>().enabled = true;
                         GetItem = false;
-                        run.SetBool("Stop", false);
+                        Effect.SetActive(false);
+                        run.SetBool("Jump", false);
                     }
                     else
                     {
@@ -119,6 +120,19 @@ namespace GameHaven.RunGame
                 run.SetBool("Over", true);
             }
 
+            
+           /* Vector3 pos1 = Camera.main.WorldToViewportPoint(Caracter.GetComponent<Transform>().position + new Vector3(-1,-1,0));
+            Vector3 pos2 = Camera.main.WorldToViewportPoint(Caracter.GetComponent<Transform>().position + new Vector3(1, -1, 0));
+            Vector3 pos3 = Camera.main.WorldToViewportPoint(Caracter.GetComponent<Transform>().position + new Vector3(1, 1, 0));
+            Vector3 pos4 = Camera.main.WorldToViewportPoint(Caracter.GetComponent<Transform>().position + new Vector3(-1, 1, 0));
+            Vector3 pos5 = Camera.main.WorldToViewportPoint(Caracter.GetComponent<Transform>().position);
+            pos1 = CameraOut(pos1);
+            pos2 = CameraOut(pos2);
+            pos3 = CameraOut(pos3);
+            pos4 = CameraOut(pos4);
+
+            Caracter.GetComponent<Transform>().position = Camera.main.ViewportToWorldPoint((pos1+pos2+pos3+pos4)/4);*/
+
         }
 
         void OnTriggerEnter2D(Collider2D other)
@@ -131,6 +145,7 @@ namespace GameHaven.RunGame
             {
                 audio = other.GetComponent<AudioSource>();
                 coin = other.GetComponent<Animator>();
+                audio.volume = SoundManager.Instance.SFXVolume;
                 audio.Play();
                 coin.SetBool("Get", true);
 
@@ -140,7 +155,7 @@ namespace GameHaven.RunGame
                 {
                     GetItem = true;
                     gameManager.ItemScore(15);
-                    run.SetBool("Stop", true);
+                    run.SetBool("Jump", true);
                 }
                 else
                 {
@@ -153,5 +168,35 @@ namespace GameHaven.RunGame
         {
             run.SetBool("Stop", false);
         }
+
+        /*public Vector3 CameraOut(Vector3 pos)
+        {
+            if (pos.x < 0f)
+            {
+                pos.x = 0.000001f;
+                dir.x = 0f;
+            }
+            if (pos.x > 1f)
+            {
+                pos.x = 0.999999f;
+                dir.x = 0f;
+            }
+
+            if (pos.y < 0f)
+            {
+                pos.y = 0.000001f;
+                dir.x = 0f;
+            }
+
+            if (pos.y > 1f)
+            {
+                pos.y = 0.999999f;
+                dir.x = 0f;
+            }
+
+            return pos;
+
+        }*/
+
     }
 }
