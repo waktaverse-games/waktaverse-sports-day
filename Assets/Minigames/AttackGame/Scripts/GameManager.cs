2deb6df35ce3052b2c5290a0c66831b1b56aecb6
streetmeow <joohee2008@cau.ac.kr>
2022-12-04 01:11:30 +0900

    아마 끝?

--- Assets/Minigames/AttackGame/Scripts/GameManager.cs
+++ Assets/Minigames/AttackGame/Scripts/GameManager.cs
@@ -71,7 +71,7 @@ namespace GameHeaven.AttackGame
         void Start()
         {
             // _enemyTypes = new string[7] {"monkey", "gorani", "fox", "cat", "pigeon", "bat", "dog"};
-            _enemyTypes = new string[7] {"pigeon", "gorani", "fox", "cat", "pigeon", "bat", "dog"};
+            _enemyTypes = new string[7] {"monkey", "gorani", "cat", "fox", "bat", "dog", "pigeon"};
             stageStartAnim = stageStart.GetComponent<Animator>();
             stageStartAnim.enabled = false;
             _hammerSpawned = false;
