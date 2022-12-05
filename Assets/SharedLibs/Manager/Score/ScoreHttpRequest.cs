7a23731912a79bdadf5ca96ba0f6fa4b2cba2a79
zhdlxh48 <zhdlxh48@gmail.com>
2022-12-06 05:30:44 +0900

    Implement server connect with http rest

---
+++ Assets/SharedLibs/Manager/Score/ScoreHttpRequest.cs
@@ -0,0 +1,55 @@
+﻿using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace SharedLibs
+{
+    public class ScoreHttpRequest
+    {
+        public static async void PostScore(string gameTitle, string gameType, string userName, int score)
+        {
+            var request = (HttpWebRequest)WebRequest.Create("http://localhost:3000/");
+            request.Method = "POST";
+            request.ContentType = "application/json";
+            request.BeginGetRequestStream((ar) =>
+            {
+                var requestStream = request.EndGetRequestStream(ar);
+                var writer = new StreamWriter(requestStream);
+                writer.Write("{\"game_title\":\"" + gameTitle + "\",\"game_type\":\"" + gameType + "\",\"user_name\":\"" + userName + "\",\"user_score\":" + score + "}");
+                writer.Flush();
+                requestStream.Close();
+                request.BeginGetResponse((ar2) =>
+                {
+                    var response = (HttpWebResponse)request.EndGetResponse(ar2);
+                    var responseStream = response.GetResponseStream();
+                    var reader = new StreamReader(responseStream);
+                    var responseString = reader.ReadToEnd();
+                    responseStream.Close();
+                    response.Close();
+                }, null);
+            }, null);
+        }
+        
+        public static async Task<string> GetScores(string gameTitle, string gameType, int count)
+        {
+            string result = "";
+            var request = (HttpWebRequest)WebRequest.Create($"http://localhost:3000/?title={gameTitle}&type={gameType}&count={count}");
+            request.Method = "GET";
+            var res = request.BeginGetResponse((ar) =>
+            {
+                var response = (HttpWebResponse)request.EndGetResponse(ar);
+                var responseStream = response.GetResponseStream();
+                var reader = new StreamReader(responseStream);
+                var responseString = reader.ReadToEnd();
+                result = responseString;
+                responseStream.Close();
+                response.Close();
+            }, null);
+            while (res.IsCompleted == false)
+            {
+                await Task.Delay(100);
+            }
+            return result;
+        }
+    }
+}
