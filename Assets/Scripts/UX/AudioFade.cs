52150e9442decc48e8cd62f0f0b2918a89d2941c
zhdlxh48 <zhdlxh48@gmail.com>
2022-12-10 03:29:12 +0900

    Change Prologue and Epilogue scene working animation to logic

---
+++ Assets/Scripts/UX/AudioFade.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using SharedLibs;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace GameHeaven.UIUX
+{
+    public class AudioFade : MonoBehaviour
+    {
+        [SerializeField] private AudioSource targetPlayer;
+        
+        [SerializeField] private float fadeInTime;
+        [SerializeField] private float fadeOutTime;
+        
+        [SerializeField] private bool muteOnStart;
+        private float originalVolume;
+        
+        [SerializeField] private UnityEvent onFadeInComplete;
+        [SerializeField] private UnityEvent onFadeOutComplete;
+        
+        private void Start()
+        {
+            originalVolume = targetPlayer.volume;
+            if (muteOnStart) targetPlayer.volume = 0;
+        }
+        
+        public void FadeIn()
+        {
+            StartCoroutine(FadeAudio(targetPlayer, fadeInTime, 0f, originalVolume, () => onFadeInComplete?.Invoke()));
+        }
+        
+        public void FadeOut()
+        {
+            StartCoroutine(FadeAudio(targetPlayer, fadeOutTime, targetPlayer.volume, 0f, () => onFadeOutComplete?.Invoke()));
+        }
+        
+        private static IEnumerator FadeAudio(AudioSource audioSource, float duration, float startVolume, float endVolume, Action onComplete = null)
+        {
+            float currentTime = 0;
+            while (currentTime < duration)
+            {
+                currentTime += Time.deltaTime;
+                audioSource.volume = Mathf.Lerp(startVolume, endVolume, currentTime / duration);
+                yield return null;
+            }
+            audioSource.volume = endVolume;
+            
+            onComplete?.Invoke();
+        }
+    }
+}
---
+++ Assets/Scripts/UX/UIPositionMover.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections;
+using System.Threading;
+using System.Threading.Tasks;
+using SharedLibs;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace GameHeaven.UIUX
+{
+    [System.Serializable]
+    public class UIPositionPoint
+    {
+        [SerializeField] private RectTransform pointTransform;
+        [SerializeField] private float moveTime;
+        [SerializeField] private float moveDelay;
+        [SerializeField] private bool isSmooth;
+        [SerializeField] private AudioClip endSfx;
+
+        public Vector3 PointPosition => pointTransform.position;
+        public float MoveTime => moveTime;
+        public float MoveDelay => moveDelay;
+        public bool IsSmooth => isSmooth;
+        public AudioClip EndSfx => endSfx;
+    }
+    
+    public class UIPositionMover : MonoBehaviour
+    {
+        [SerializeField] private RectTransform targetTransform;
+        [SerializeField] private RectTransform pivotTransform;
+        
+        [SerializeField] private UIPositionPoint[] points;
+        
+        [SerializeField] private bool isScroll;
+        
+        [SerializeField] private UnityEvent onStartMove;
+        [SerializeField] private UnityEvent onEndMove;
+
+        [SerializeField] private AudioSource bgmPlayer;
+        [SerializeField] private AudioSource sfxPlayer;
+
+        private void Awake()
+        {
+            bgmPlayer.playOnAwake = false;
+
+            bgmPlayer.volume = SoundManager.Instance.BGMVolume;
+            sfxPlayer.volume = SoundManager.Instance.SFXVolume;
+        }
+
+        private void Start()
+        {
+            bgmPlayer.Play();
+            StartCoroutine(StartMove());
+        }
+
+        private IEnumerator StartMove()
+        {
+            onStartMove?.Invoke();
+            for (var i = 0; i < points.Length; i++)
+            {
+                var point = points[i];
+                var isOver = false;
+                StartCoroutine(MovePositionToPoint(point.PointPosition, point.MoveTime, point.MoveDelay,
+                    () => { isOver = true;}, point.IsSmooth));
+                yield return new WaitUntil(() => isOver);
+                if (point.EndSfx)
+                {
+                    sfxPlayer.PlayOneShot(point.EndSfx);
+                }
+            }
+            onEndMove?.Invoke();
+        }
+        
+        private IEnumerator MovePositionToPoint(Vector3 point, float time, float delay, Action onEndMove, bool isSmooth = false)
+        {
+            var elapsedTime = 0f;
+            var startPosition = targetTransform.position;
+            var moveVec = isScroll ? (pivotTransform.position - point) : (point - pivotTransform.position);
+
+            yield return new WaitForSeconds(delay);
+            
+            while (elapsedTime < time)
+            {
+                elapsedTime += Time.deltaTime;
+                targetTransform.position = isSmooth
+                    ? startPosition + moveVec * Mathf.SmoothStep(0, 1, elapsedTime / time)
+                    : Vector3.Lerp(startPosition, startPosition + moveVec, elapsedTime / time);
+                yield return null;
+            }
+
+            targetTransform.position = startPosition + moveVec;
+            pivotTransform.position = point;
+            onEndMove?.Invoke();
+        }
+    }
+}
